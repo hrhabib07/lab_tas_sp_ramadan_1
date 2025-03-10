cfd6ddb97cf4acf3df998e76cd422c4de853d0b5
hrhabib07 <mdhabibur.hr7@gmail.com>
2025-03-10 10:55:54 +0600

    new problem sovled

--- O_Compare.cpp
+++ O_Compare.cpp
@@ -5,15 +5,11 @@ int main()
 {
     string s1, s2;
     cin >> s1 >> s2;
-    int flag = 0;
-    int l1 = s1.size(), l2 = s2.size();
 
-    if (l1 < l2 && !flag)
-        s1;
-    if (l1 < l2 && flag)
-        s2;
-    if (l1 > l2 && flag)
-        s2;
+    if (s1 < s2)
+        cout << s1;
+    else
+        cout << s2;
 
     return 0;
 }
